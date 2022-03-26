ae384502fb6915f50b3c8a7cd4a6f0ecaa502893
Movind26 <85275381+Movind26@users.noreply.github.com>
2022-03-27 00:31:04 +0300

    Update tasks.js
    Calculator implemented

--- tasks.js
+++ tasks.js
@@ -12,3 +12,56 @@ function concatStrings(argument, separator = null) {
   
   return sum.toString();
 }
+
+class Calculator {
+  constructor(x, y) {
+    if (!x || !Number.isInteger(x) || !y || !Number.isInteger(y)) {
+      throw new Error("Values are empty or not a number. Enter valid values");
+    }
+    this.x = x;
+    this.y = y;
+  }
+
+  setX(valueX) {
+    if (!valueX || !Number.isInteger(valueX)) {
+      throw new Error("Value X is empty or not a number. Enter valid X");
+    }
+    this.x = valueX;
+  }
+
+  setY(valueY) {
+    if (!valueY || !Number.isInteger(valueY)) {
+      throw new Error("Value Y is empty or not a number. Enter valid Y");
+    }
+    this.y = valueY;
+  }
+
+  logSum(x = this.x, y = this.y) {
+    return x + y;
+  }
+
+  logMul(x = this.x, y = this.y) {
+    return x * y;
+  }
+
+  logSub(x = this.x, y = this.y) {
+    return x - y;
+  }
+
+  logDiv(x = this.x, y = this.y) {
+    return x / y;
+  }
+}
+
+// const calculator = new Calculator(12, 3);
+
+// calculator.setX(10);
+// calculator.setY(5);
+
+// console.log("Sum", calculator.logSum());
+// console.log("Mul", calculator.logMul());
+// console.log("Sub", calculator.logSub());
+// console.log("Div", calculator.logDiv());
+
+// const test = calculator.logSum;
+// console.log("Test", test(3, 5));
